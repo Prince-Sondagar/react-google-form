3a949788ad7ab8168bf24d2eac38c762ae0e3350
Prince-Sondagar <sondagarprince1@gmail.com>
2024-12-02 22:58:20 +0530

    designed header and workign on sidebar design

--- src/layouts/DashboardLayout/index.tsx
+++ src/layouts/DashboardLayout/index.tsx
@@ -11,7 +11,7 @@ const DashboardLayout = () => {
             {/* <AuthGuard> */}
                 <Header />
                 <Outlet />
-                <Footer />
+                {/* <Footer /> */}
             {/* </AuthGuard> */}
 
         </>
