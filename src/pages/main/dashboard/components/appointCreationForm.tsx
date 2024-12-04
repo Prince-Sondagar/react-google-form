1398ec332f29c8483662da28295487eae4050ba6
Prince-Sondagar <sondagarprince1@gmail.com>
2024-12-04 23:24:29 +0530

    add the field in the schema

--- src/pages/main/dashboard/components/appointCreationForm.tsx
+++ src/pages/main/dashboard/components/appointCreationForm.tsx
@@ -1,8 +1,9 @@
-import { Box, Card, CardContent, CardHeader, Divider, FormControlLabel, Grid, InputLabel, Radio, RadioGroup, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Divider, FormControlLabel, FormHelperText, Grid, InputLabel, Radio, RadioGroup, Typography } from "@mui/material";
 import Input from "../../../../components/ui/input";
 import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup'
 import { AppointmentCreationSchema } from "../../../../utils/schema";
+import CommonButton from "../../../../components/ui/button";
 
 
 
@@ -27,16 +28,32 @@ const AppointmentCreationForm = () => {
 
     const {
         control,
-        formState: { errors }
+        formState: { errors },
+        handleSubmit
     } = useForm({
         resolver: yupResolver(AppointmentCreationSchema),
         defaultValues: {
             firstName: "",
             lastName: "",
             age: 0,
-            gender: ""
+            gender: "",
+            email: "",
+            phoneNumber: "",
+            maritalStatus: "",
+            bloodGroup: "",
+            country: "",
+            state: "",
+            city: "",
+            houseNumber: "",
+            street: "",
+            zipCode: "",
+            termsAndCondition: false
         }
     })
+
+    const handleSaveSubmit = () => {
+
+    }
 
     return (
         <Card sx={{ minWidth: 400 }}>
@@ -47,81 +64,171 @@ const AppointmentCreationForm = () => {
             <Divider />
 
             <CardContent>
-                <form>
+                <form onSubmit={handleSubmit(handleSaveSubmit)}>
                     <Grid container spacing={2}>
                         <Grid item xl={6} xs={12} md={6} sm={12}>
                             <Box>
                                 <InputLabel>First Name</InputLabel>
-                                <Controller
-                                    name="firstName"
-                                    control={control}
-                                    render={({ field }) => (
-                                        <Input
-                                            fullWidth
-                                            placeholder="Enter First Name"
-                                            error={!!errors.firstName}
-                                            helperText={errors.firstName?.message}
-                                            {...field}
-                                        />
-                                    )} />
                             </Box>
+                            <Controller
+                                name="firstName"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        placeholder="Enter First Name"
+                                        error={!!errors.firstName}
+                                        helperText={errors.firstName?.message}
+                                        {...field}
+                                    />
+                                )}
+                            />
                         </Grid>
                         <Grid item xl={6} xs={12} md={6} sm={12}>
                             <Box>
                                 <InputLabel>Last Name</InputLabel>
-                                <Controller
-                                    name="lastName"
-                                    control={control}
-                                    render={({ field }) => (
-                                        <Input
-                                            fullWidth
-                                            placeholder="Enter Last Name"
-                                            error={!!errors.lastName}
-                                            helperText={errors.lastName?.message}
-                                            {...field}
-                                        />
-                                    )} />
                             </Box>
+                            <Controller
+                                name="lastName"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        placeholder="Enter Last Name"
+                                        error={!!errors.lastName}
+                                        helperText={errors.lastName?.message}
+                                        {...field}
+                                    />
+                                )}
+                            />
                         </Grid>
                         <Grid item xl={6} xs={12} md={6} sm={12}>
                             <Box>
                                 <InputLabel>Age</InputLabel>
-                                <Controller
-                                    name="age"
-                                    control={control}
-                                    render={({ field }) => (
-                                        <Input
-                                            fullWidth
-                                            type="number"
-                                            placeholder="Enter Your Age"
-                                            error={!!errors.age}
-                                            helperText={errors.age?.message}
-                                            {...field}
-                                            sx={{ color: "black" }}
-                                        />
-                                    )} />
                             </Box>
+                            <Controller
+                                name="age"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        type="number"
+                                        placeholder="Enter Your Age"
+                                        error={!!errors.age}
+                                        helperText={errors.age?.message}
+                                        {...field}
+                                        sx={{ color: "black" }}
+                                    />
+                                )}
+                            />
                         </Grid>
                         <Grid item xl={6} xs={12} md={6} sm={12}>
                             <Box>
                                 <InputLabel>Gender</InputLabel>
-                                <Controller
-                                    name="gender"
-                                    control={control}
-                                    render={({ field }) => (
-                                        <RadioGroup
-                                            row
-                                            aria-labelledby="demo-row-radio-buttons-group-label"
-                                            name="row-radio-buttons-group"
-                                        >
-                                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                                        </RadioGroup>
-                                    )} />
                             </Box>
+                            <Controller
+                                name="gender"
+                                control={control}
+                                render={({ field }) => (
+                                    <RadioGroup
+                                        row
+                                        aria-labelledby="demo-row-radio-buttons-group-label"
+                                        name="row-radio-buttons-group"
+                                    >
+                                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                        <FormControlLabel value="other" control={<Radio />} label="Other" />
+                                    </RadioGroup>
+                                )}
+                            />
+                            {errors.gender?.message && <FormHelperText>{errors.gender?.message}</FormHelperText>}
+                        </Grid>
+                        <Grid item xl={6} xs={12} md={6} sm={12}>
+                            <Box>
+                                <InputLabel>Email ID</InputLabel>
+                            </Box>
+                            <Controller
+                                name="email"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        type="email"
+                                        placeholder="Enter Your Email ID"
+                                        error={!!errors.email}
+                                        helperText={errors.email?.message}
+                                        {...field}
+                                    />
+                                )}
+                            />
+
+                        </Grid>
+                        <Grid item xl={6} xs={12} md={6} sm={12}>
+                            <Box>
+                                <InputLabel>Phone Number</InputLabel>
+                            </Box>
+                            <Controller
+                                name="phoneNumber"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        type="number"
+                                        placeholder="Phone Number is required"
+                                        error={!!errors.phoneNumber}
+                                        helperText={errors.phoneNumber?.message}
+                                        {...field}
+                                    />
+                                )}
+                            />
+                        </Grid>
+                        <Grid item xl={6} xs={12} md={6} sm={12}>
+                            <Box>
+                                <InputLabel>Marital Status</InputLabel>
+                            </Box>
+                            <Controller
+                                name="maritalStatus"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        type="number"
+                                        placeholder="MaritalStatus is required"
+                                        error={!!errors.maritalStatus}
+                                        helperText={errors.maritalStatus?.message}
+                                        {...field}
+                                    />
+                                )}
+                            />
+                        </Grid>
+                        <Grid item xl={6} xs={12} md={6} sm={12}>
+                            <Box>
+                                <InputLabel>Blood Group</InputLabel>
+                            </Box>
+                            <Controller
+                                name="bloodGroup"
+                                control={control}
+                                render={({ field }) => (
+                                    <Input
+                                        fullWidth
+                                        placeholder="BloodGroup is required"
+                                        error={!!errors.bloodGroup}
+                                        helperText={errors.bloodGroup?.message}
+                                        {...field}
+                                    />
+                                )}
+                            />
+
                         </Grid>
                     </Grid>
+                    <Box>
+                        <CommonButton
+                            variant="primary"
+                            type="submit"
+                        >
+                            Save
+                        </CommonButton>
+                    </Box>
                 </form>
             </CardContent>
         </Card>
--- src/utils/schema/index.ts
+++ src/utils/schema/index.ts
@@ -4,5 +4,16 @@ export const AppointmentCreationSchema = yup.object().shape({
     firstName: yup.string().required({ message: "firstName is required" }),
     lastName: yup.string().required({ mesage: "lastName is rquired" }),
     age: yup.number().required("Age is required").min(18, "Age must be grather than 18 years").max(60, "Age must be less than 60 years"),
-    gender: yup.string().required("Gender is required")
+    gender: yup.string().required("Gender is required"),
+    email: yup.string().required("Email is required"),
+    phoneNumber: yup.string().required("Phone Number is required"),
+    maritalStatus: yup.string().required("Marital Status is required"),
+    bloodGroup: yup.string().required("Blood Group is required"),
+    country: yup.string().required("Country is required"),
+    state: yup.string().required("State is required"),
+    city: yup.string().required("City is required"),
+    houseNumber: yup.string().required("House Number is  required"),
+    street: yup.string().required("Street is required"),
+    zipCode: yup.string().required("Zip Code is required"),
+    termsAndCondition: yup.boolean().oneOf([true], "TermsAndCondition is required").required("TermsAndCondition is required")
 })
